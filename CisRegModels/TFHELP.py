1142ae6b8dd7fe2c80dbadddf3ac2d9f5739a503
Keertana Krishnan <kkrish@uw.edu>
2019-04-29 18:08:13 -0700

    Added comments to tfhelp.py

--- CisRegModels/TFHELP.py
+++ CisRegModels/TFHELP.py
@@ -1,3 +1,8 @@
+## This file consists of various functions with the goal of reading and
+## writing items as part of a batch
+## Used for converting sequences into binary (OHC)
+## and into vectors for tensorflow
+
 from threading import Thread;
 from . import MYUTILS
 import numpy as np;
@@ -6,15 +11,18 @@ import sys
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import math_ops
 
+## This function determines the learning rate of the model
+## Uses tensorflow (machine learning tool)
 def cycle_learning_rate(learning_rate_low, learning_rate_high, global_step, period, name=None):
 	if global_step is None:
 		raise ValueError("global_step is required for cycle_learning_rate.")
 	with ops.name_scope(name, "CycleLR",
 											[learning_rate_high, learning_rate_low, global_step,
 											 period]) as name:
+        ## Converting np arrays to tensor
 		learning_rate_high = ops.convert_to_tensor(learning_rate_high, name="learning_rate_high")
 		learning_rate_low = ops.convert_to_tensor(learning_rate_low, name="learning_rate_low")
-		
+		## Initialize (gloabl) variables and types
 		dtype = learning_rate_high.dtype
 		global_step = math_ops.cast(global_step, dtype)
 		period = math_ops.cast(period, dtype)
@@ -22,7 +30,8 @@ def cycle_learning_rate(learning_rate_low, learning_rate_high, global_step, peri
 		cycle_stage = tf.abs(tf.subtract(tf.floormod(global_step,tf.multiply(math_ops.cast(2.0,dtype),period)),period))
 		return tf.add(learning_rate_low, tf.multiply(tf.subtract(period,cycle_stage),step_size), name=name)
 
-#abstract object for polymorphism 
+#abstract object for polymorphism
+## Define class and function for getting specific batches
 class BasicBatchGetter:
 	def getNextBatch(self):
 		self.curThread.join();
@@ -33,10 +42,13 @@ class BasicBatchGetter:
 		self.curThread.start()
 		return(curX, curY, curR);
 
-
 #asynchronous batch getting for one hot encoding of sequences
+## Define class and batch for getting encoded sequences
+## OHC format (binary)
+## No specific return - value storage mechanism
 class BatchGetterOneHot(BasicBatchGetter):
 	def __init__(self, inFP, batchSize, numRuns, seqLen):
+        ## Define sequence parameters
 		self.inFP = inFP;
 		self.batchSize = batchSize;
 		self.numRuns= numRuns;
@@ -44,31 +56,41 @@ class BatchGetterOneHot(BasicBatchGetter):
 		self.curFH = MYUTILS.smartGZOpen(self.inFP,'r')
 		self.curThread = Thread(target = self.prepareNextBatch);
 		self.curThread.start()
+    ## function for prepare another batch based on a previous one
 	def prepareNextBatch(self):
 		self.nextBatchX = np.zeros((self.batchSize,4,self.seqLen,1))
 		self.nextBatchY = np.zeros((self.batchSize))
+        ## Initialize counter
 		b=0
 		while b < self.batchSize:
 			line = self.curFH.readline()
 			if line =="":
 				if self.numRuns==1:
+                    ## Update variables
 					self.nextBatchX = self.nextBatchX[0:b,:,:,:]
 					self.nextBatchY = self.nextBatchY[0:b]
 					self.numRuns-=1;
 					return;
 				self.curFH.close();
+                ## Read file
 				self.curFH = MYUTILS.smartGZOpen(self.inFP,'r')
 				self.numRuns-=1;
 				line = self.curFH.readline()
+            ## Open files
 			if line is None or line[0]=="#": continue
 			curData = np.fromstring(line, dtype=float, sep="\t")
 			self.nextBatchY[b]=curData[0];
 			self.nextBatchX[b,:,:,0] = curData[1:].reshape((4,self.seqLen))
+            ## Update counter
 			b+=1
 
 
+## Function using the basicbatchgetter class
+## Optimizes original function
+## Same as batchgetteronehot but not encoded
 class BatchGetter(BasicBatchGetter):
 	def __init__(self, inFP, batchSize, numRuns,numTFs, numKds):
+        ## Defining parameters
 		self.inFP = inFP;
 		self.batchSize = batchSize;
 		self.numRuns= numRuns;
@@ -101,6 +123,9 @@ class BatchGetter(BasicBatchGetter):
 				self.nextBatchX[b,0:min(self.numKds,len(curKds)),t-1] = curKds[0:min(self.numKds,len(curKds))];
 			b+=1
 
+## New batchgetter class
+## Updated numKds values
+## Otherwise same code as before
 class BatchGetterFixedNumKds(BatchGetter):
 	def prepareNextBatch(self):
 		self.nextBatchX = np.zeros((self.batchSize,self.numKds,self.numTFs)) +np.log(99999.0);
@@ -124,7 +149,9 @@ class BatchGetterFixedNumKds(BatchGetter):
 			self.nextBatchX[b,:,:] = np.transpose(curData[1:len(curData)].reshape((self.numTFs,self.numKds)))
 			b+=1
 
-
+## Function with similar code as above
+## Updated class, calling basicbatchgetter
+## Used to conver sequences to vectors for tensorflow
 class BatchGetterSeq2Vec(BasicBatchGetter):
 	def __init__(self, inFP, batchSize, numRuns,seqLen, kmer2index, wordLen):
 		self.inFP = inFP;
@@ -136,7 +163,7 @@ class BatchGetterSeq2Vec(BasicBatchGetter):
 		self.curFH = MYUTILS.smartGZOpen(self.inFP,'r')
 		self.curThread = Thread(target = self.prepareNextBatch);
 		self.curThread.start()
-		
+
 	def prepareNextBatch(self):
 		self.nextBatchX = np.zeros((self.batchSize,self.seqLen-self.wordLen+1)).astype("int32");
 		self.nextBatchY = np.zeros((self.batchSize))
@@ -163,5 +190,4 @@ class BatchGetterSeq2Vec(BasicBatchGetter):
 			for si in range(0,self.seqLen-self.wordLen+1):
 				self.nextBatchX[b,si] = self.kmer2index[curSeq[si:(si+self.wordLen)]] #fill X with the indeces of the various k-mers
 			b+=1
-
 
